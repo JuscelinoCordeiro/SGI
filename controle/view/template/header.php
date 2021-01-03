8bd6463e5ef33ccf83850a46c17147705a652959
Juscelino Cordeiro <jfilhodev@gmail.com>
2021-01-03 20:53:16 -0300

    divisao do index em views

---
+++ controle/view/template/header.php
@@ -0,0 +1,14 @@
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta name="keywords" content="Sistema de Gestão">
+        <title>SGI</title>
+        <link rel="stylesheet" href="css/estilo.css">
+        <link rel="stylesheet" href="css/bootstrap.css">
+        <script type="text/javascript" src="js/jquery-1.11.3.js"></script>
+        <script type="text/javascript" src="js/bootstrap-dropdown.js"></script>
+        <script type="text/javascript" src="js/bootstrap.js"></script>
+        <script type="text/javascript" src="js/bootstrap.min.js"></script>
+    </head>
+    <body>
