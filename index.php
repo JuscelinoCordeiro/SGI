72f4f2cfe1a4c101e9cbf8cdd1a4498e582b5236
Juscelino Cordeiro <jfilhodev@gmail.com>
2021-01-03 13:44:35 -0300

    ativação de menu dropdown

--- index.php
+++ index.php
@@ -8,7 +8,7 @@
         <link rel="stylesheet" href="css/estilo.css">
         <link rel="stylesheet" href="css/bootstrap.css">
         <script type="text/javascript" src="js/jquery-1.11.3.js"></script>
-        <!--<script type="text/javascript" src="js/bootstrap-dropdown.js"></script>-->
+        <script type="text/javascript" src="js/bootstrap-dropdown.js"></script>
         <script type="text/javascript" src="js/bootstrap.js"></script>
         <script type="text/javascript" src="js/bootstrap.min.js"></script>
     </head>
