b80d773f2b39ac494fda077a10b4966f525dda1d
Juscelino Cordeiro <jfilhodev@gmail.com>
2021-01-03 21:25:07 -0300

    update version

--- controle/view/template/footer.php
+++ controle/view/template/footer.php
@@ -5,7 +5,7 @@
         </div>
         <div class="row-fluid">
             <div id="rodape" class="col-xs-12 well well-lg">  
-                <center><span class="rodape-login">Versão 0.1.0 (dev) - Copyright &COPY; Todos os direitos reservados</span></center>
+                <center><span class="rodape-login">Versão 0.1.1 (dev) - Copyright &COPY; Todos os direitos reservados</span></center>
             </div>            
         </div>
     </body>
