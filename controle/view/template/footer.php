8c04a8c3675c520f48197228c68779af6ca84d28
Juscelino Cordeiro <jfilhodev@gmail.com>
2021-01-03 21:01:58 -0300

    definição da release 0.1.0

--- controle/view/template/footer.php
+++ controle/view/template/footer.php
@@ -5,7 +5,7 @@
         </div>
         <div class="row-fluid">
             <div id="rodape" class="col-xs-12 well well-lg">  
-                <center><span class="rodape-login">Copyright &COPY; Todos os direitos reservados</span></center>
+                <center><span class="rodape-login">Versão 0.1.0 - Copyright &COPY; Todos os direitos reservados</span></center>
             </div>            
         </div>
     </body>
